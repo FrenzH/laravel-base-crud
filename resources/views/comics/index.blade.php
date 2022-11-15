21f011eecb1544226d735aa5a7f92969dd4413a5
FrenzH <franciattom99@gmail.com>
2022-11-15 15:30:06 +0000

    show route + create route

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,10 @@
 @foreach ($comics as $comic)
+<a href="{{route('comics.show', $comic->id)}}">
+    <p>{{$comic['title']}}</p>
 
-<p>{{$comic['title']}}</p>
+</a>
+
+
 
 @endforeach
+<a href="{{route('comics.create')}}">add comics</a>
